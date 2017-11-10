fed66553f2f0a860853e5915ce8640af8fb7f0ad
sandip bhaumik <sandipbhauwmik2010@gmail.com>
2017-11-10 11:02:32 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,29 @@
+var i,j;
+for( i=1;i<=5;i++) 
+{
+    for(j=1;j<=i;j++)
+    {
+    console.log('*');
+  
+    }
+    console.log('<br>');
+}
+
+
+
+
+// v = 15;
+
+// function f1() {
+//     "use strict";
+//     var v = "Hi I am a strict mode script";
+//     console.log(v)
+// }
+
+// f1();
+
+
+
+// "use strict";
+
+// v ='Hi I am strict mode script!';
